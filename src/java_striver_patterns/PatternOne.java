671ff652b593262cd4e75604728ce884b9cbb0fb
abhijithms123 <appu.ms.001@gmail.com>
2025-02-19 23:55:46 +0530

    first problem

---
+++ src/java_striver_patterns/PatternOne.java
@@ -0,0 +1,18 @@
+package java_striver_patterns;
+
+public class PatternOne {
+
+	public static void main(String[] args) {
+		System.out.println("one");
+		
+		for(int j=0;j<5;j++) {
+			for(int i=0;i<4;i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
